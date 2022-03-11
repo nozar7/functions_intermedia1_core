811dd8786a183ae3a5b1edbf0a7c96fb1fbc4e29
nozar7 <rolin.espinozafelipe@gmail.com>
2022-03-11 01:45:26 -0500

    update code to remote repository

---
+++ funcionesIntermedias.py
@@ -0,0 +1,15 @@
+x = [ [5,2,3], [10,8,9] ] 
+x[1][0]=15
+estudiantes = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+estudiantes[0]['last_name']='Bryant'
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Messi', 'Ronaldo', 'Rooney']
+}
+directorio_deportes["fútbol"][0]='Andres'
+z = [ {'x': 10, 'y': 20} ]
+z[0]['y']=30
+print(z)
